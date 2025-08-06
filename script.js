a09a48807f845d944e57b6e05d96842df22a0444
yesubabu7788 <raaviyesubabu77@gmail.com>
2025-08-06 21:08:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add or remove 'scrolled' class on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
